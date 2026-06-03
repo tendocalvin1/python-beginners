8fcb432ad3fb7dc38426508afc8b7f90956a878e
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-06-03 21:26:28 +0300

    feat: enhance string operations examples and add method demonstrations

--- strings.py
+++ strings.py
@@ -4,10 +4,74 @@ name = "Tendo"
 country = "Uganda"
 email = "tendo@gmail.com"
 
-print(name)
-print(country)
-print(email)
+print(name[0])
+print(country[0])
+print(email[0])
 
 print(type(name))
 print(type(country))
 print(type(email))
+
+language = "python"
+print(language[-1])
+
+# string methods
+print(len(language))
+
+# string concatenation
+# combining strings
+first_name = "Tendo"
+last_name = "Calvin"
+title = "SWE"
+
+full_name = title + " " + first_name + " " + last_name
+print(full_name)
+
+# string methods
+# methods are functions attached to strings
+
+# 1. upper()
+# conerts to uppercase
+name = "tendo"
+
+print(name.upper())
+
+# 2. lower()
+name = "TENDO"
+
+print(name.lower())
+
+
+# 3. title
+name = "tendo calvin"
+
+print(name.title())
+
+
+# 3. strip()
+# removes spaces from both ends
+name = "   Tendo   "
+
+print(name.strip())
+
+# 4. replace()
+sentence = "I love Java"
+
+print(sentence.replace("Java", "Python"))
+
+
+framework = "Django"
+
+print(framework.replace("Django", "FastAPI"))
+
+
+email = "tendo@gmail.com"
+
+print("@" in email)
+
+# looping through strings
+# a string is a sequence
+name_one = "Tendo"
+
+for letter in name_one:
+    print(letter)
