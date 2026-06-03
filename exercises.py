ec080e95692e0e2cc2c811d391bef9fea1636d32
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-06-03 08:46:30 +0300

    refactor: comment out example functions for clarity and maintainability

--- exercises.py
+++ exercises.py
@@ -1,57 +1,68 @@
-# exercises on *args and **kwargs
+# # exercises on *args and **kwargs
 
 
-def order_food(*args):
-    print(args)
+# def order_food(*args):
+#     print(args)
     
     
-order_food("chips", "chicken", "pork", "rice")
+# order_food("chips", "chicken", "pork", "rice")
 
 
 
-def student(**kwargs):
-    print(kwargs)
+# def student(**kwargs):
+#     print(kwargs)
     
-student(
-    name ='Tendo Calvin',
-    age = 24,
-    country = "Uganda"
-)
+# student(
+#     name ='Tendo Calvin',
+#     age = 24,
+#     country = "Uganda"
+# )
 
 
-def student1(*args, **kwargs):
-    print(args)
-    print(kwargs)
+# def student1(*args, **kwargs):
+#     print(args)
+#     print(kwargs)
 
-student1(
-    "Python",
-    "Django",
-    name="Tendo",
-    country="Uganda"
-)
+# student1(
+#     "Python",
+#     "Django",
+#     name="Tendo",
+#     country="Uganda"
+# )
 
 
-def show_courses(*args):
+# def show_courses(*args):
 
-    for course in args:
-        print(course)
+#     for course in args:
+#         print(course)
         
-show_courses("Python","Django","PostgreSQL")
-# printing the tuple : print(args) result : ("Python","Django","PostgreSQL")
+# show_courses("Python","Django","PostgreSQL")
+# # printing the tuple : print(args) result : ("Python","Django","PostgreSQL")
 
-# looping through the tuple: 
-# for course in args:
-#    print(course)
+# # looping through the tuple: 
+# # for course in args:
+# #    print(course)
 
-# output: 
-# Python
-# Django
-# PostgreSQL
+# # output: 
+# # Python
+# # Django
+# # PostgreSQL
 
 
-def show_skills(*args):
-    for skill in args:
-        print(skill)
+# def show_skills(*args):
+#     for skill in args:
+#         print(skill)
     
 
-show_skills("python", "django", "postgreSQL")   
+# show_skills("python", "django", "postgreSQL") 
+
+
+def add_numbers(*args):
+    total = 0
+    
+    for number in args:
+        total += number
+    
+    return total
+
+print(add_numbers(10,20,30))
