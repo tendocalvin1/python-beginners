91c854687c6ec795df3e619ef07cd265d95fcec5
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-06-05 08:00:09 +0300

    feat: add advanced examples for nested dictionaries, skills management, and safe access methods

--- dictionaries.py
+++ dictionaries.py
@@ -83,3 +83,93 @@ cpu_stats = {
 
 for key, value in cpu_stats.items():
     print(f"{key}: {value}")
+    
+    
+    
+# Nested dictionaries
+# accessing nested data
+user = {
+    "name": "Tendo",
+    "address": {
+        "city": "Kampala",
+        "country": "Uganda"
+    }
+}
+
+print(user["address"]["city"])
+print (user["address"]["country"])
+
+
+developer1 = {
+    "name": "Tendo Calvin",
+    "skills" : ["Data engineer", "full-stack engineer", "digital creator"]
+}
+
+print(developer1["skills"][1])
+
+
+# challenge 4
+server = {
+    "hostname": "web-01",
+    "specs": {
+        "cpu": 8,
+        "memory": 32
+    }
+}
+
+
+print(server["specs"]["cpu"])
+print(server["specs"]["memory"])
+
+# challenge 5
+developer = {
+    "name": "Tendo",
+    "skills": [
+        "Python",
+        "Django",
+        "PostgreSQL"
+    ]
+}
+
+
+print(developer["skills"][0])
+print(developer["skills"][1])
+
+
+
+# advanced dictionary skills
+# keys()
+user = {
+    "name": "Tendo",
+    "country": "Uganda"
+}
+
+print(user.keys())
+
+# values() returns all values
+print(user.values())
+
+
+# items()
+# returns key value pairs
+for key, value in user.items():
+    print(key, value)
+
+
+# safe access with .get()
+user = {
+    "name": "Tendo"
+}
+
+user.get("age")
+
+request_data = {
+    "username": "Tendo",
+    "email": "tendo@gmail.com"
+}
+
+username = request_data.get("username")
+email = request_data.get("email")
+
+print(username)
+print(email)
