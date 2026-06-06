6c23883eb95420223c1329444a89edefcbca73a1
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-06-06 16:38:44 +0300

    feat: update BankAccount methods to include parameters and improve output messages

--- OOP/bankAccount.py
+++ OOP/bankAccount.py
@@ -6,13 +6,18 @@ class BankAccount:
         self.balance = balance
         
     
-    def deposit(self):
-        print()
+    def deposit(amount):
+        # deposit = balance + deposit
+        print(f"The amount of money deposited in the bank is {amount}")
         
         
-    def withdraw(self):
-        print()
+    def withdraw(amount):
+        # withdraw = balance - withdraw
+        print(f"The amount of money withdrawn from the bank is {amount}")
         
+    def current_balance(amount):
+        # current_balance = (amount + deposit) - withdraw
+        print(balance)
 
-    def display_balance(self):
-        print()
+    def display_balance():
+        print(f" The balance left is {current }")
