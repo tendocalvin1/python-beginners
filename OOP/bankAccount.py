d750a16d1ec8221df7f0b0d8de47740480b98cf8
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-06-06 21:00:59 +0300

    feat: refactor BankAccount methods for clarity and functionality

--- OOP/bankAccount.py
+++ OOP/bankAccount.py
@@ -4,20 +4,14 @@ class BankAccount:
     def __init__(self, owner, balance):
         self.owner = owner
         self.balance = balance
-        
-    
-    def deposit(amount):
-        # deposit = balance + deposit
-        print(f"The amount of money deposited in the bank is {amount}")
-        
-        
-    def withdraw(amount):
-        # withdraw = balance - withdraw
-        print(f"The amount of money withdrawn from the bank is {amount}")
-        
-    def current_balance(amount):
-        # current_balance = (amount + deposit) - withdraw
-        print(balance)
 
-    def display_balance():
-        print(f" The balance left is {current }")
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"The amount deposited is {amount}")
+
+    def withdraw(self, amount):
+        self.balance -= amount
+        print(f"The amount withdrawn is {amount}")
+
+    def display_balance(self):
+        print(f"Current balance: {self.balance}")
