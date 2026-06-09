3fa8af713f12706c89525048f06e1b678a810f91
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-06-09 14:47:40 +0300

    feat: expand explanations on polymorphism and inheritance with method overriding examples

--- OOP/revision.py
+++ OOP/revision.py
@@ -2,4 +2,29 @@
 # That is polymorphism and Inheritance
 # Inheritance is a situation where someone adopts the characteristics of another person
 # for example in Inheritance, a child can adopt the characters of the father ie his eyes, his legs, his skin tone
-# Now in polymorphism, 
+# Now in polymorphism
+
+# Method overriding
+
+# The Core Idea
+# The parent class already has a method.
+# The child class creates a method with the exact same name.
+# Python uses the child's version instead of the parent's version.
+
+
+# examples in Inheritance
+
+class Parent():
+    def __init__(self,name):
+        self.name = name
+        
+    def introduce(self):
+            print(f"My name is {self.name}")
+        
+        
+class Child(Parent):
+       pass
+   
+child1 = Child("Tendo Calvin")
+child1.introduce()
+
