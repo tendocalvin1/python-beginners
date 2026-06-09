1c4bc68c091f967d6ff30370d600ca2cf70bcd99
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-06-09 14:31:54 +0300

    feat: add introductory comments on polymorphism and inheritance concepts

---
+++ OOP/revision.py
@@ -0,0 +1,5 @@
+# I want to first go through what I have learnt so far 
+# That is polymorphism and Inheritance
+# Inheritance is a situation where someone adopts the characteristics of another person
+# for example in Inheritance, a child can adopt the characters of the father ie his eyes, his legs, his skin tone
+# Now in polymorphism, 
