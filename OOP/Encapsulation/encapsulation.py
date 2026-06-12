7ed27d1b91fdc7b2d7a372ae941c0dca270953f2
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-06-12 17:01:41 +0300

    feat: add initial definition and explanation of encapsulation with real-world analogy

---
+++ OOP/Encapsulation/encapsulation.py
@@ -0,0 +1,31 @@
+# Simple Definition
+
+# Encapsulation means:
+
+# Bundling data and behavior together while controlling access to the data.
+
+# In simpler language:
+
+# Protect the object's important information and force people to interact with it through approved methods.
+
+# Real World Analogy
+
+# Think about an ATM.
+
+# You can:
+
+# Deposit money
+# Withdraw money
+# Check balance
+
+# But can you open the ATM and directly edit:
+
+# balance = 1,000,000
+
+# No.
+
+# The internal money is protected.
+
+# You interact through approved actions.
+
+# That's encapsulation.
