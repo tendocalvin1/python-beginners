5292dc83216df8bf4daaf888583c4c9f83d2b75d
Tendo Calvin <125687687+tendocalvin1@users.noreply.github.com>
2026-06-18 09:06:30 +0300

    feat: add composition example with Engine, PetrolEngine, ElectricEngine, and Car classes

---
+++ OOP/composition/composition.py
@@ -0,0 +1,39 @@
+# Composition
+class Engine:
+    def start(self):
+        print("Engine started")
+        
+class PetrolEngine(Engine):
+    def start(self):
+        print("Petrol engine started")
+
+
+class ElectricEngine(Engine):
+    def start(self):
+        print("Electric engine started")
+        
+class Car:
+    def __init__(self, engine):
+        self.engine = engine
+        
+    def start(self):
+        self.engine.start()
+        print("Car moving")
+    
+# tesla = Car('')
+# tesla.start()
+
+
+# Why Composition Is Powerful
+
+# Suppose tomorrow:
+# PetrolEngine
+# ElectricEngine
+# HybridEngine
+
+# You can swap engines.
+
+
+        
+tesla = Car(ElectricEngine())
+toyota = Car(PetrolEngine())
